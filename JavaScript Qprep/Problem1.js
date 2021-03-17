10b1f5d7c49f119588c9eb45b8ec5a23945ae89a
Amit Sinha <aamitsinha456@gmail.com>
2021-03-18 00:10:00 +0530

    Qprep

---
+++ JavaScript Qprep/Problem1.js
@@ -0,0 +1,9 @@
+//Problem Description
+//Complete the greet() function to return a string "Hello World"
+
+function greet() {
+    return "Hello World";
+}
+module.exports = greet;
+
+console.log(greet());
