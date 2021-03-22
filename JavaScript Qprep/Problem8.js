1f03c2b13e6bf56970bf17f803d43e28a21de422
Amit Sinha <aamitsinha456@gmail.com>
2021-03-22 22:34:25 +0530

    Object in JavaScript

---
+++ JavaScript Qprep/Problem8.js
@@ -0,0 +1,25 @@
+/*Problem Description
+
+The personDetails() function is called with two parameters, a and b. These represent the first name and last name of a person.
+
+Complete the function personDetail() such that it returns an object modelling a person that has the following properties:
+
+    firstName: this value is equal to parameter a.
+
+    lastName: this value is equal to parameter b.
+
+    fullName: this value is equal to concatenation of a and b separated by a space.*/
+
+    function personDetail(a, b) {
+        return {
+            firstName: a,
+            lastName: b,
+            fullName: a + " " + b
+    
+        }
+    }
+    
+    module.exports = personDetail;
+    
+    console.log(personDetail("John", "Smith"));
+
